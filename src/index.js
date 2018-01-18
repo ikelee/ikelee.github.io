45492d121a8758cabc5964431e53ece7dd2be4cb
ike lee <ikelee@ikes-MacBook-Pro.local>
2018-01-18 02:31:19 -0500

    Animation glitch fix

--- src/index.js
+++ src/index.js
@@ -24,23 +24,34 @@ const styles = {
   },
 };
 
-this.state = {animate: false}
-function changeState() {
-  this.state.animate = true
-}
-
 var sectionStyle = {
-    width: "100%",
-    height: "100%",
-  };
+  width: "100%",
+  height: "100%",
+};
 
-const MyComponent = () => (
-  <SwipeableViews enableMouseEvents animateHeight={this.state.animate} onClick={changeState}>
-    <About style={Object.assign({}, styles.slide, styles.slide1)} style={sectionStyle}/>
-    <Artist style={Object.assign({}, styles.slide, styles.slide2)} style={sectionStyle}/>
-    <Developer style={Object.assign({}, styles.slide, styles.slide3)} style={sectionStyle}/>
-    <Journalism style={Object.assign({}, styles.slide, styles.slide3)} style={sectionStyle}/>
-  </SwipeableViews>
-);
+class MyComponent extends React.Component {
+  constructor(props) {
+      super(props);
+      this.state = {animate: false};
+
+      this.changeState = this.changeState.bind(this);
+  }
+  
+  changeState() {
+    console.log(this)
+    this.setState({animate: true})
+  }
+
+  render() {
+    return (
+      (<SwipeableViews enableMouseEvents animateHeight={this.state.animate} onClick={this.changeState}>
+        <About style={Object.assign({}, styles.slide, styles.slide1)} style={sectionStyle}/>
+        <Artist style={Object.assign({}, styles.slide, styles.slide2)} style={sectionStyle}/>
+        <Developer style={Object.assign({}, styles.slide, styles.slide3)} style={sectionStyle}/>
+        <Journalism style={Object.assign({}, styles.slide, styles.slide3)} style={sectionStyle}/>
+      </SwipeableViews>
+      ));
+    }
+  }
 
 ReactDOM.render(<MyComponent/>, document.querySelector('#root'))
