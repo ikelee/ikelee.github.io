b21c182302d1e8dae896d9c3f126f185be146351
Eek Seung Lee <e76lee@uwaterloo.ca>
2019-05-22 00:01:05 -0400

    Potentially buggy google analytics

--- src/index.js
+++ src/index.js
@@ -162,6 +162,15 @@ class MyComponent extends React.Component {
     return (
       (<Router>
         <div>
+	  <!-- Global site tag (gtag.js) - Google Analytics -->
+	    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-140654519-1"></script>
+	    <script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-140654519-1');
+	  </script>
           <NavBar style={{
             top: `${this.state.slide}`,
           }}>
